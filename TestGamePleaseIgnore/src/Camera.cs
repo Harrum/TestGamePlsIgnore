49e719f36c5f748a4f9966aa1c64037dce1adc16
Harm <Harm@pc>
2015-10-27 17:51:44 +0100

    - Added Camera wich automatically adjusts the viewport, can have a BaseEntity to focus on.

---
+++ TestGamePleaseIgnore/src/Camera.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestGamePleaseIgnore.src.Entity;
+
+namespace TestGamePleaseIgnore.src
+{
+    public class Camera
+    {
+        private BaseEntity Actor;
+        private static Camera Instance;
+
+        public float X { private set; get; }
+        public float Y { private set; get; }
+
+        public static Camera GetInstance()
+        {
+            if (Instance == null)
+                Instance = new Camera();
+            return Instance;
+        }
+
+        private Camera()
+        {
+            Actor = null;
+        }
+
+        public void SetActor(BaseEntity actor)
+        {
+            this.Actor = actor;
+        }
+
+        public void RemoveActor()
+        {
+            this.Actor = null;
+        }
+
+        public void Update()
+        {
+            if (this.Actor != null)
+            {
+                this.X = Actor.X - (Config.SCREEN_WIDTH / 2);
+                this.Y = Actor.Y - (Config.SCREEN_HEIGHT / 2);
+            }
+            else
+            {
+                this.X = Config.SCREEN_WIDTH / 2;
+                this.Y = Config.SCREEN_HEIGHT / 2;
+            }
+        }
+    }
+}
