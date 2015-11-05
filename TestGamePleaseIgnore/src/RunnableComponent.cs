41aa5136d30e57860831721636a36b04c7cdb814
Harm <Harm@pc>
2015-11-05 17:44:11 +0100

    Added the Quadtree for collision checks, Every update tick the quadtree gets rebuild. After that a collision check is done for all the entities that need them.

--- TestGamePleaseIgnore/src/RunnableComponent.cs
+++ TestGamePleaseIgnore/src/RunnableComponent.cs
@@ -14,13 +14,19 @@ namespace TestGamePleaseIgnore.src
         public static ViewportF VIEWPORT;
         private static List<BaseEntity> Entities;
         private List<BaseEntity> VisibleEntities;
+        private static List<BaseEntity> CollidableEntities;
+        private Camera GameCamera;
+        QuadTree Quad;
 
         public void InitBase()
         {
             VIEWPORT = new ViewportF(0, 0, Config.SCREEN_WIDTH, Config.SCREEN_HEIGHT);
             Entities = new List<BaseEntity>();
             VisibleEntities = new List<BaseEntity>();
+            CollidableEntities = new List<BaseEntity>();
             Initialize();
+            GameCamera = Camera.GetInstance();
+            Quad = new QuadTree(0, new RectangleF(VIEWPORT.X, VIEWPORT.Y, VIEWPORT.Width, VIEWPORT.Height));
         }
 
         public abstract void Initialize();
@@ -34,6 +40,8 @@ namespace TestGamePleaseIgnore.src
         /// </summary>
         protected void UpdateViewPort()
         {
+            VIEWPORT.X = GameCamera.X;
+            VIEWPORT.Y = GameCamera.Y;
             VisibleEntities.Clear();
             foreach(BaseEntity e in Entities)
             {
@@ -45,6 +53,33 @@ namespace TestGamePleaseIgnore.src
             VisibleEntities = VisibleEntities.OrderBy(ent => ent.Layer).ToList<BaseEntity>();
         }
 
+        private void UpdateQuadTree()
+        {
+            Quad.Clear();
+            for(int i = 0; i < Entities.Count; i++)
+            {
+                Quad.Insert(Entities[i]);
+            }
+        }
+
+        private void UpdateCollisions()
+        {
+            List<BaseEntity> returnObjects = new List<BaseEntity>();
+            for(int i = 0; i < CollidableEntities.Count; i++)
+            {
+                returnObjects.Clear();
+                Quad.Retrieve(returnObjects, CollidableEntities[i].Hitbox);
+                returnObjects.Remove(CollidableEntities[i]);
+                CollidableEntities[i].HandleCollisions(returnObjects);
+            }
+        }
+
+        public static void ClearEntities()
+        {
+            CollidableEntities.Clear();
+            Entities.Clear();
+        }
+
         /// <summary>
         /// Adds an entity to the game's render and update loop
         /// </summary>
@@ -54,16 +89,25 @@ namespace TestGamePleaseIgnore.src
             Entities.Add(entity);
         }
 
+        public static void AddCollidableEntity(BaseEntity entity)
+        {
+            Entities.Add(entity);
+            CollidableEntities.Add(entity);
+        }
+
         /// <summary>
         /// Updates all the entites.
         /// </summary>
         /// <param name="elapsedTime">The elapsed time in ms since the last update</param>
         public virtual void Update(long elapsedTime)
-        {
+        {   
             for (int i = 0; i < Entities.Count; i++)
             {
                 Entities[i].Update(elapsedTime);
             }
+            UpdateQuadTree();
+            UpdateCollisions();
+            GameCamera.Update();
         }
 
         /// <summary>
@@ -76,6 +120,10 @@ namespace TestGamePleaseIgnore.src
             for (int i = 0; i < VisibleEntities.Count; i++)
             {
                 VisibleEntities[i].Draw(g);
+            }
+            if (Config.DEBUG_MODE == DebugMode.DISPLAY_HITBOX)
+            {
+                Quad.Draw(g);
             }
         }
     }
