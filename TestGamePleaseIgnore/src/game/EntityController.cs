6a48822ece288a8eab42a57f7649b2bb0d9646c3
Harm <Harm@pc>
2015-12-02 14:38:52 +0100

    Added the entity controller which will be used to control the entities at a later stage.

---
+++ TestGamePleaseIgnore/src/game/EntityController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TestGamePleaseIgnore.src.Entity;
+
+namespace TestGamePleaseIgnore.src.game
+{
+    public class EntityController
+    {
+        private static EntityController instance;
+        private ControllableEntity puppet;
+
+        public static EntityController GetInstance()
+        {
+            if (instance == null)
+                instance = new EntityController();
+            return instance;
+        }
+
+        private EntityController()
+        {
+            puppet = null;
+        }
+
+        public void SetPuppetEntity(ControllableEntity puppet)
+        {
+            this.puppet = puppet;
+        }
+
+        public void RemovePuppetEntity()
+        {
+            this.puppet = null;
+        }
+
+        public void Update(long elapsedTime)
+        {
+            if (puppet != null)
+            {
+                if (InputController.IsKeyDown((int)Keys.A))
+                    puppet.MoveLeft();
+                else if (InputController.IsKeyDown((int)Keys.D))
+                    puppet.MoveRight();
+                if (InputController.IsKeyDown((int)Keys.W))
+                    puppet.MoveUp();
+                else if (InputController.IsKeyDown((int)Keys.S))
+                    puppet.MoveDown();
+            }
+        }
+    }
+}
