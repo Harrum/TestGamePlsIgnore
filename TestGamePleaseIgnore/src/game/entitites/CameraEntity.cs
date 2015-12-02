a2b0d6a3f0e5f76f96e31328cbb2ad5ed3931334
Harm <Harm@pc>
2015-12-02 14:36:52 +0100

    Created camera entity to test the camera scrolling functionality.

---
+++ TestGamePleaseIgnore/src/game/entitites/CameraEntity.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TestGamePleaseIgnore.src.Entity;
+
+namespace TestGamePleaseIgnore.src.game.entitites
+{
+    public class CameraEntity : ControllableEntity
+    {
+        private const float CAM_SPEED = 2f;
+
+        public CameraEntity(float x, float y, float width, float height) : base(x, y, width, height)
+        {
+            base.SetTexture(GameTextures.CameraTexture);
+            base.Layer = 5;
+        }
+
+        public override void MoveLeft()
+        {
+            this.SpeedX = -CAM_SPEED;
+        }
+
+        public override void MoveRight()
+        {
+            this.SpeedX = CAM_SPEED;
+        }
+
+        public override void MoveUp()
+        {
+            this.SpeedY = CAM_SPEED;
+        }
+
+        public override void MoveDown()
+        {
+            this.SpeedY = -CAM_SPEED;
+        }
+
+        public override void Update(long elapsedTime)
+        {
+            if (InputController.IsKeyDown((int)Keys.A))
+                this.X -= CAM_SPEED;
+            else if (InputController.IsKeyDown((int)Keys.D))
+                this.X += CAM_SPEED;
+            if (InputController.IsKeyDown((int)Keys.W))
+                this.Y -= CAM_SPEED;
+            else if (InputController.IsKeyDown((int)Keys.S))
+                this.Y += CAM_SPEED;
+            base.Update(elapsedTime);
+        }
+    }
+}
