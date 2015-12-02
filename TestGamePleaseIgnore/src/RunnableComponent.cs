13015843b743172f9de775fb240c008041aaad31
Harm <Harm@pc>
2015-12-02 14:51:00 +0100

    - Added a property which holds the size of the game's area. - Added an identity matrix and it's mirror equivelant. - Added a method to change the game area size. - Improved the viewport so it wil correctly hold the entities on the side. - Moved the update viewport call to the update loop instead of the render loop. As it is not needed to keep updating the viewport if the game hasn't updated yet. - Moved the mirror handling to this main drawing loop. It will now check if an entity is mirrored and then change the matrix to the mirrored one.

--- TestGamePleaseIgnore/src/RunnableComponent.cs
+++ TestGamePleaseIgnore/src/RunnableComponent.cs
@@ -12,29 +12,48 @@ namespace TestGamePleaseIgnore.src
     public abstract class RunnableComponent
     {
         public static ViewportF VIEWPORT;
+        private RectangleF GAME_AREA_SIZE;
+
         private static List<BaseEntity> Entities;
         private List<BaseEntity> VisibleEntities;
         private static List<BaseEntity> CollidableEntities;
         private Camera GameCamera;
         QuadTree Quad;
+        
+        Matrix3x2 ViewportIDMatrix;
+        Matrix3x2 ViewportIDMatrixMirror;
 
         public void InitBase()
         {
             VIEWPORT = new ViewportF(0, 0, Config.SCREEN_WIDTH, Config.SCREEN_HEIGHT);
+            GAME_AREA_SIZE = new RectangleF(0, 0, Config.SCREEN_WIDTH, Config.SCREEN_HEIGHT);
+            ViewportIDMatrix = Matrix.Identity;
+            ViewportIDMatrixMirror = new Matrix3x2();
+            ViewportIDMatrixMirror.M11 = -1; ViewportIDMatrixMirror.M12 = 0;
+            ViewportIDMatrixMirror.M21 = 0; ViewportIDMatrixMirror.M22 = 1;
+            ViewportIDMatrixMirror.M31 = 0; ViewportIDMatrixMirror.M32 = 0;
+
+
             Entities = new List<BaseEntity>();
             VisibleEntities = new List<BaseEntity>();
             CollidableEntities = new List<BaseEntity>();
             Initialize();
             GameCamera = Camera.GetInstance();
-            Quad = new QuadTree(0, new RectangleF(VIEWPORT.X, VIEWPORT.Y, VIEWPORT.Width, VIEWPORT.Height));
+            Quad = new QuadTree(0, GAME_AREA_SIZE);
         }
 
         public abstract void Initialize();
 
         public abstract void LoadContent(RenderTarget g);
 
+        protected void SetGameAreaSize(RectangleF area)
+        {
+            this.GAME_AREA_SIZE = area;
+            Quad.UpdateBounds(GAME_AREA_SIZE);
+        }
+
         /// <summary>
-        /// Updates the viewport, creates a list wich contains all the visible entites inside the viewport.
+        /// Updates the viewport, creates a list which contains all the visible entites inside the viewport.
         /// The list is sorted by Entity.Layer so it wil perform drawing operations in layers,
         /// Layer 1 first, 2 second, etc.
         /// </summary>
@@ -45,7 +64,8 @@ namespace TestGamePleaseIgnore.src
             VisibleEntities.Clear();
             foreach(BaseEntity e in Entities)
             {
-                if(VIEWPORT.Bounds.Contains(e.X, e.Y))
+                if(e.X + e.Width >= VIEWPORT.X && e.X <= VIEWPORT.X + VIEWPORT.Width
+                    && e.Y + e.Height >= VIEWPORT.Y && e.Y <= VIEWPORT.Y + VIEWPORT.Height)
                 {
                     VisibleEntities.Add(e);
                 }
@@ -65,12 +85,13 @@ namespace TestGamePleaseIgnore.src
         private void UpdateCollisions()
         {
             List<BaseEntity> returnObjects = new List<BaseEntity>();
-            for(int i = 0; i < CollidableEntities.Count; i++)
+
+            foreach(BaseEntity e in CollidableEntities)
             {
                 returnObjects.Clear();
-                Quad.Retrieve(returnObjects, CollidableEntities[i].Hitbox);
-                returnObjects.Remove(CollidableEntities[i]);
-                CollidableEntities[i].HandleCollisions(returnObjects);
+                Quad.Retrieve(returnObjects, e.Hitbox);
+                returnObjects.Remove(e);
+                e.HandleCollisions(returnObjects);
             }
         }
 
@@ -89,6 +110,11 @@ namespace TestGamePleaseIgnore.src
             Entities.Add(entity);
         }
 
+        /// <summary>
+        /// Adds an entity which needs to check for collision tot he game's render and update loop,
+        /// By using this method it is not neccessary to use AddEntity too.
+        /// </summary>
+        /// <param name="entity">The entity to add.</param>
         public static void AddCollidableEntity(BaseEntity entity)
         {
             Entities.Add(entity);
@@ -100,7 +126,8 @@ namespace TestGamePleaseIgnore.src
         /// </summary>
         /// <param name="elapsedTime">The elapsed time in ms since the last update</param>
         public virtual void Update(long elapsedTime)
-        {   
+        {
+            UpdateViewPort();
             for (int i = 0; i < Entities.Count; i++)
             {
                 Entities[i].Update(elapsedTime);
@@ -111,20 +138,50 @@ namespace TestGamePleaseIgnore.src
         }
 
         /// <summary>
+        /// Set the matrix to the identity matrix transated to the viewport.
+        /// This causes the screen to move along with the camera at the center.
+        /// </summary>
+        /// <param name="g">The rendertarget which holds the matrix.</param>
+        public void SetMatrixViewportID(RenderTarget g)
+        {
+            Vector2 viewPortTranslation = new Vector2(-VIEWPORT.X, -VIEWPORT.Y);
+            ViewportIDMatrix = Matrix.Identity;
+            ViewportIDMatrix.TranslationVector = viewPortTranslation;
+            g.Transform = ViewportIDMatrix;
+        }
+
+        /// <summary>
         /// Draws all the entities wich are visible in the viewport
         /// </summary>
         /// <param name="g">The RenderTarget used to perform the drawing operations.</param>
         public virtual void Draw(RenderTarget g)
         {
-            UpdateViewPort();
-            for (int i = 0; i < VisibleEntities.Count; i++)
+            //At the start set the matrix to the identy viewport matrix
+            SetMatrixViewportID(g);
+
+            //Start drawing all the entities.
+            foreach (BaseEntity ent in VisibleEntities)
             {
-                VisibleEntities[i].Draw(g);
+                //If an entity is mirror the matrix needs to get mirrored too.
+                if(ent.Mirrored)
+                {
+                    Vector2 mirroredviewPortTranslation = new Vector2(-VIEWPORT.X + ent.X + ent.Width + ent.X, -VIEWPORT.Y);
+                    ViewportIDMatrixMirror.TranslationVector = mirroredviewPortTranslation;
+                    g.Transform = ViewportIDMatrixMirror;
+                }
+
+                //Actual drawing of the entity
+                ent.Draw(g);
+
+                //If the entity was mirrored then the matrix needs to be reset back to the identity viewport matrix.
+                if (ent.Mirrored)
+                    SetMatrixViewportID(g);
             }
             if (Config.DEBUG_MODE == DebugMode.DISPLAY_HITBOX)
             {
                 Quad.Draw(g);
             }
+            g.Transform = Matrix.Identity;
         }
     }
 }
