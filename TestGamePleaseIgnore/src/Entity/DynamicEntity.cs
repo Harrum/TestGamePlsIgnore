2dbc491f8930669c3630497ef23c44aaaaecc340
Harm <Harm@pc>
2016-01-25 16:14:28 +0100

    Class wich handles the movement of entities.

---
+++ TestGamePleaseIgnore/src/Entity/DynamicEntity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpDX.Direct2D1;
+
+namespace TestGamePleaseIgnore.src.Entity
+{
+    public class DynamicEntity : BaseEntity
+    {
+        protected float SpeedX { get; set; }
+        protected float SpeedY { get; set; }
+
+        public DynamicEntity(float x, float y, float width, float height) : base(x, y, width, height)
+        {
+
+        }
+
+        public override void Update(long elapsedTime)
+        {  
+            X += SpeedX;
+            Y += SpeedY;
+            base.Update(elapsedTime);
+            base.UpdateHitbox();
+        }
+    }
+}
--- TestGamePleaseIgnore/src/game/entitites/Penguin.cs
+++ TestGamePleaseIgnore/src/game/entitites/Penguin.cs
@@ -3,11 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SharpDX.Direct2D1;
+using TestGamePleaseIgnore.src.Entity;
+using TestGamePleaseIgnore.src.game.entitites.Effects;
 
 namespace TestGamePleaseIgnore.src.game.entitites
 {
-    public class Penguin
+    public class Penguin : ControllableEntity
     {
+        private const float WIDTH = 62;
+        private const float HEIGHT = 60;
 
+        private List<CollisionEffect> collisionEffects;
+
+        public Penguin(float x, float y) : base(x, y, WIDTH, HEIGHT)
+        {
+            base.SetTexture(GameTextures.PenguinIdle);
+            collisionEffects = new List<CollisionEffect>();
+        }
+
+        public override void HandleCollisions(List<BaseEntity> collisions)
+        {
+            foreach (CollisionEffect effect in collisionEffects)
+            {
+                Game.RemoveEntity(effect);
+            }
+            collisionEffects.Clear();
+            base.HandleCollisions(collisions);
+        }
+
+        protected override void Collision(BaseEntity col, Direction dir)
+        {
+            base.Collision(col, dir);
+            CollisionEffect effect = new CollisionEffect(col, dir);
+            collisionEffects.Add(effect);
+            Game.AddEntity(effect);
+        }
+
+        public override void Draw(RenderTarget g)
+        {
+            base.Draw(g);
+        }
     }
 }
